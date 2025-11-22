70b1715788394dca938ee9d619ca16a75bcecca1
Vincent Cheng <vincent.chengyunsheng@gmail.com>
2025-11-22 17:32:29 +0800

    Add dataset source information to the homepage

--- app/Homepage.py
+++ app/Homepage.py
@@ -7,3 +7,4 @@ st.set_page_config(
 st.title("Recipe Reviews & User Feedback Analysis")
 st.sidebar.success("Select a page above.")
 
+st.markdown("This analysis is done using the [Recipe Reviews and User Feedback](https://archive.ics.uci.edu/dataset/911/recipe+reviews+and+user+feedback+dataset) dataset from UCI Machine Learning Repository.")
