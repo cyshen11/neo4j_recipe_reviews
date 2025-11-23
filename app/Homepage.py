f2f3ee9799431582182932edcd4f2f93ab10997b
Vincent Cheng <vincent.chengyunsheng@gmail.com>
2025-11-23 09:46:59 +0800

    Updated homepage description

--- app/Homepage.py
+++ app/Homepage.py
@@ -7,4 +7,8 @@ st.set_page_config(
 st.title("Recipe Reviews & User Feedback Analysis")
 st.sidebar.success("Select a page above.")
 
-st.markdown("This analysis is done using the [Recipe Reviews and User Feedback](https://archive.ics.uci.edu/dataset/911/recipe+reviews+and+user+feedback+dataset) dataset from UCI Machine Learning Repository.")
+st.markdown("""
+This app demonstrates different analyses that can be performed on the [Recipe Reviews and User Feedback](https://archive.ics.uci.edu/dataset/911/recipe+reviews+and+user+feedback+dataset) dataset from the UCI Machine Learning Repository.
+
+The dataset is ingested into a Neo4j graph database, and the analyses are performed using Cypher queries.
+""")
